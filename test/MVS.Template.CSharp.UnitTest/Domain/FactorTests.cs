2d759ab9e9a26de80cc294c7a0f35f946bebee40
Matheus Souza <mvs@mvsouza.com>
2018-12-20 15:23:46 -0200

    First unit test.

---
+++ test/MVS.Template.CSharp.UnitTest/Domain/FactorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVS.Template.CSharp.Domain.Entities;
+using Xunit;
+
+namespace MVS.Template.CSharp.UnitTest.Domain
+{
+    public class FactorTests
+    {
+        [Fact]
+        public void Single_factor_solution()
+        {
+            var factor = new Factor("10");
+            Assert.Equal(10, factor.Solve());
+        }
+    }
+}
