c9799f814b2dcfc4dc57b70a4452ae510c826010
mvsouza <mvs@mvsouza.com>
2019-11-24 22:39:16 -0300

    Refactor duplicated tests.

--- test/MVS.Template.CSharp.UnitTest/API/SolveControllerTests.cs
+++ test/MVS.Template.CSharp.UnitTest/API/SolveControllerTests.cs
@@ -14,28 +14,23 @@ namespace MVS.Template.CSharp.UnitTest.API
     public class SolveControllerTests
     {
         [Fact]
-        public async Task Should_send_SolveCommand()
+        public async Task Should_send_SolveCommand_Sync()
         {
-            var solvedValue = 10.0;
-            var mediatr = new Mock<IMediator>();
-            mediatr.Setup(m => m.Send<double>(It.IsAny<IRequest<double>>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(solvedValue));
-            var controller = new SolveController(mediatr.Object);
-            var result = await controller.Post("(10*1)");
-            Assert.Equal(solvedValue, Assert.IsType<OkObjectResult>(result).Value);
-
+            await Should_send_SolveCommand((solvedValue) => () => Task.FromResult(solvedValue));
         }
         [Fact]
         public async Task Should_send_SolveCommand_Async()
         {
-            var solvedValue = 10.0;
-            var mediatr = new Mock<IMediator>();
-            var tcs = new TaskCompletionSource<double>();
-            
-            mediatr.Setup(m => m.Send<double>(It.IsAny<IRequest<double>>(), It.IsAny<CancellationToken>())).Returns(async () => {
+            await Should_send_SolveCommand((solvedValue) => (async () => {
                 await Task.Delay(1);
                 return solvedValue;
-            });
-            tcs.TrySetResult(solvedValue);
+            }));
+        }
+        public async Task Should_send_SolveCommand(Func<double,Func<Task<double>>> solveResult)
+        {
+            var solvedValue = 10.0;
+            var mediatr = new Mock<IMediator>();
+            mediatr.Setup(m => m.Send<double>(It.IsAny<IRequest<double>>(), It.IsAny<CancellationToken>())).Returns(solveResult(solvedValue));
             var controller = new SolveController(mediatr.Object);
             var result = await controller.Post("(10*1)");
             Assert.Equal(solvedValue, Assert.IsType<OkObjectResult>(result).Value);
