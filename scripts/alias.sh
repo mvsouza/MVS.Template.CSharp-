a13b5570ee60a2c3fc0df2b65dce8814e76a6788
mvsouza <mvs@mvsouza.com>
2020-06-26 00:08:00 -0300

    Moved Scaffolding to another repo.

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -4,8 +4,6 @@ alias dnt='dotnet test'
 alias dnbc='dotnet build --no-incremental --force'
 
 alias dnc='dotnet aspnet-codegenerator -p .'
-alias cc='rm -r ~/.nuget/packages/mvs.scaffolding; rm -r ../../artifacts'
-alias bp='dnbc ../../tools/MVS.Scaffolding/MVS.Scaffolding.csproj && dotnet pack ../../tools/MVS.Scaffolding -o ../../artifacts --version-suffix dev-1 && dotnet restore'
 _dotnet_zsh_complete()
 {
   local completions=("$(dotnet complete "$words")")
--- tools/MVS.Scaffolding/BundleModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVS.Scaffolding
-{
-    public class BundleModel
-    {
-        public string Namespace { get;set; }
-        public string ClassName { get;set; }
-    }
-}
